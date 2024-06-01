6def7717fa2d75ceb4aaac2660a2832359bfbad9
Manishkr2703 <krmanish2703@gmail.com>
2024-06-01 18:44:56 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,21 @@
+package airlinereservationsystem;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinereservationsystem", "root", "Manish");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+    
